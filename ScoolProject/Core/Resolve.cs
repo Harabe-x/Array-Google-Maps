844f70c4779e3e30e8e239ae01b724f78814f3cd
Harabe-x <harabex1321@gmail.com>
2023-01-14 00:19:40 +0100

    Improved Move Methods
    Also added  _visted array which contains visited places.

--- ScoolProject/Core/Resolve.cs
+++ ScoolProject/Core/Resolve.cs
@@ -9,10 +9,25 @@ namespace SchoolProject.Core
 {
     internal class Resolve
     {
+        #region Fields
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int cost { get; set; }
+
         /// <summary>
         /// Array of chars which contains board to solve
         /// </summary>
         private char[,] _board;
+
+        /// <summary>
+        ///  Array thats stores visited places 
+        /// </summary>
+        private Dictionary<int, int> _visited;
+
+        #endregion
+        #region ctor
 
         /// <summary>
         /// CTOR LOL 
@@ -21,13 +36,19 @@ namespace SchoolProject.Core
         internal Resolve(char[,] board)
         {
             _board = board;
+            _visited = new Dictionary<int, int>();
         }
 
+        #endregion
+        #region Private Methods
         /// <summary>
         /// Check if the specified position is board border 
         /// </summary>
         /// <param name="DrawnIndex"></param>
         /// <returns></returns>
+
+       
+
         internal static bool BorderCheck(Char DrawnIndex)
         {
             if (DrawnIndex == '+')
@@ -38,12 +59,24 @@ namespace SchoolProject.Core
             return false;
         }
 
-        /// <summary>
-        /// Prints board to console.
-        /// </summary>
-        /// <param name="_board"></param>
-        internal void Show() => BoardMethods.Show(_board);
+        private bool VisitedCheck(int key, int value)
+        {
+            foreach (KeyValuePair<int, int> item in _visited)
+            {
+                if (item.Key == key && item.Value == value)
+                {
+                    return true;
+                }
+            }
 
+            return false;
+        }
+
+        private void AddVisitedPlace(int key , int value)
+        {
+            _visited.Add(key ,value);
+        }
+   
 
         /// <summary>
         /// Not implemented yet 
@@ -51,62 +84,8 @@ namespace SchoolProject.Core
         internal void Start()
         {
         }
-
-        /// <summary>
-        /// Moves start Character UP
-        /// </summary>
-        internal void MoveUp()
-        {
-            KeyValuePair<int, int> currentpos = GetStartCharPos();
-            if (!BorderCheck(_board[currentpos.Key + 1, currentpos.Value]))
-            {
-                _board[currentpos.Key, currentpos.Value] = (char)9632;
-                _board[currentpos.Key + 1, currentpos.Value] = '&';
-                Show();
-            }
-        }
-
-        /// <summary>
-        /// Moves start Character DOWN
-        /// </summary>
-        internal void MoveDown()
-        {
-            KeyValuePair<int, int> currentpos = GetStartCharPos();
-            if (!BorderCheck(_board[currentpos.Key - 1, currentpos.Value]))
-            {
-                _board[currentpos.Key, currentpos.Value] = (char)9632;
-                _board[currentpos.Key - 1, currentpos.Value] = '&';
-                Show();
-            }
-        }
-
-        /// <summary>
-        /// Moves start Character LEFT
-        /// </summary>
-        internal void MoveLeft()
-        {
-            KeyValuePair<int, int> currentpos = GetStartCharPos();
-            if (!BorderCheck(_board[currentpos.Key, currentpos.Value - 1]))
-            {
-                _board[currentpos.Key, currentpos.Value] = (char)9632;
-                _board[currentpos.Key, currentpos.Value - 1] = '&';
-                Show();
-            }
-        }
-
-        /// <summary>
-        /// Moves start Character RIGHT
-        /// </summary>
-        internal void MoveRight()
-        {
-            KeyValuePair<int, int> currentpos = GetStartCharPos();
-            if (!BorderCheck(_board[currentpos.Key, currentpos.Value + 1]))
-            {
-                _board[currentpos.Key, currentpos.Value] = (char)9632;
-                _board[currentpos.Key, currentpos.Value + 1] = '&';
-                Show();
-            }
-        }
+        #endregion
+        #region GetPosMethods
 
         /// <summary>
         /// Finds the Start Char 
@@ -161,5 +140,98 @@ namespace SchoolProject.Core
             position = new KeyValuePair<int, int>(a, b);
             return position;
         }
+
+        #endregion
+        #region MoveMethods
+
+        /// <summary>
+        /// Moves start Character UP
+        /// </summary>
+        internal void MoveUp()
+        {
+            KeyValuePair<int, int> currentpos = GetStartCharPos();
+            AddVisitedPlace(currentpos.Key - 1, currentpos.Value);
+            if (!BorderCheck(_board[currentpos.Key - 1, currentpos.Value]))
+            {
+                if (!VisitedCheck(currentpos.Key - 1, currentpos.Value ))
+                {
+                    cost += _board[currentpos.Key - 1, currentpos.Value ] - '0';
+                }
+                _board[currentpos.Key, currentpos.Value] = (char)9632;
+                _board[currentpos.Key - 1, currentpos.Value] = '&';
+                Console.WriteLine($"Total cost: {cost}");
+                AddVisitedPlace(currentpos.Key - 1, currentpos.Value );
+
+                BoardMethods.Show(_board);
+            }
+        }
+
+        /// <summary>
+        /// Moves start Character DOWN
+        /// </summary>
+        internal void MoveDown()
+        {
+            KeyValuePair<int, int> currentpos = GetStartCharPos();
+            AddVisitedPlace(currentpos.Key + 1, currentpos.Value);
+
+            if (!BorderCheck(_board[currentpos.Key + 1, currentpos.Value]))
+            {
+                if (!VisitedCheck(currentpos.Key + 1, currentpos.Value ))
+                {
+                    cost += _board[currentpos.Key + 1, currentpos.Value ] - '0';
+                }
+                _board[currentpos.Key + 1, currentpos.Value] = '&';
+                _board[currentpos.Key, currentpos.Value] = (char)9632;
+                AddVisitedPlace(currentpos.Key + 1, currentpos.Value );
+                Console.WriteLine($"Total cost: {cost}");
+                BoardMethods.Show(_board);
+            }
+        }
+
+        /// <summary>
+        /// Moves start Character LEFT
+        /// </summary>
+        internal void MoveLeft()
+        {
+            KeyValuePair<int, int> currentpos = GetStartCharPos();
+            AddVisitedPlace(currentpos.Key , currentpos.Value -1);
+
+            if (!BorderCheck(_board[currentpos.Key, currentpos.Value - 1]))
+            {
+                if (!VisitedCheck(currentpos.Key, currentpos.Value - 1))
+                {
+                    cost += _board[currentpos.Key, currentpos.Value - 1] - '0';
+                }
+                _board[currentpos.Key, currentpos.Value] = (char)9632;
+                _board[currentpos.Key, currentpos.Value - 1] = '&';
+                AddVisitedPlace(currentpos.Key , currentpos.Value + 1 );
+                Console.WriteLine($"Total cost: {cost}");
+                BoardMethods.Show(_board);
+            }
+        }
+
+        /// <summary>
+        /// Moves start Character RIGHT
+        /// </summary>
+        internal void MoveRight()
+        {
+            
+            KeyValuePair<int, int> currentpos = GetStartCharPos();
+            AddVisitedPlace(currentpos.Key - 1, currentpos.Value);
+            if (!BorderCheck(_board[currentpos.Key, currentpos.Value + 1]))
+            {
+                if (!VisitedCheck(currentpos.Key, currentpos.Value + 1))
+                {
+                    cost += _board[currentpos.Key, currentpos.Value + 1] - '0';
+                }
+                _board[currentpos.Key, currentpos.Value] = (char)9632;
+                _board[currentpos.Key, currentpos.Value + 1] = '&';
+                AddVisitedPlace(currentpos.Key , currentpos.Value + 1 );
+                Console.WriteLine($"Total cost: {cost}");
+                BoardMethods.Show(_board);
+            }
+        }
+
+        #endregion
     }
 }
--- ScoolProject/Program.cs
+++ ScoolProject/Program.cs
@@ -10,27 +10,39 @@ class Program
     internal static void Main(string[] args)
     {
         ConsoleKey key;
-        int size;
-        Animations.WriteAnimation("Enter size :", TimeSpan.FromMilliseconds(0));
-        int.TryParse(Console.ReadLine(), out size);
-        Resolve res = new Resolve(BoardMethods.FillBoard(BoardMethods.Generate(size)));
+        //int size;
+        //Animations.WriteAnimation("Enter size :", TimeSpan.FromMilliseconds(0));
+        //int.TryParse(Console.ReadLine(), out size);
+        Resolve res = new Resolve(BoardMethods.FillBoard(BoardMethods.Generate(10)));
         Console.Clear();
-        res.Show();
-        Console.ReadKey();
         Console.Clear();
         while (true)
         {
             key = Console.ReadKey().Key;
             switch (key)
             {
-                case ConsoleKey.A : res.MoveLeft(); Thread.Sleep(1000); Console.Clear(); break;
-                case ConsoleKey.D : res.MoveRight();
-                    Thread.Sleep(1000); Console.Clear(); break;
-                case ConsoleKey.W : res.MoveUp();
-                    Thread.Sleep(1000); Console.Clear(); break;
-                case ConsoleKey.S : res.MoveDown();
-                    Thread.Sleep(1000); Console.Clear(); break;
-                case ConsoleKey.Backspace : return;
+                case ConsoleKey.A :
+                    res.MoveLeft(); 
+                 //   Thread.Sleep(1000); 
+                    Console.SetCursorPosition(0,0);
+                    break;
+                case ConsoleKey.D : 
+                    res.MoveRight();
+                   // Thread.Sleep(1000); 
+                    Console.SetCursorPosition(0,0); 
+                    break;
+                case ConsoleKey.W :
+                    res.MoveUp();
+               //     Thread.Sleep(1000); 
+                    Console.SetCursorPosition(0,0);
+                    break;
+                case ConsoleKey.S :
+                    res.MoveDown();
+                //    Thread.Sleep(1000); 
+                    Console.SetCursorPosition(0,0);
+                    break;
+                case ConsoleKey.Backspace :
+                    return;
             }
         }
     }
